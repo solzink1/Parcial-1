41084cb02c431f3ce9af793a40b83d301647dd8f
Sol Zink <sol.zink@davinci.edu.ar>
2021-09-29 15:48:58 -0300

    Actualizacion API_KEY

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = "e45b39fa90da46d30883f5cc082fd869";
 
 const button = document.getElementById("sendButton");
 const contenido = document.getElementById("contenido");
